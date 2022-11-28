c05ed51478b7271a38784e88822927502c0240a6
Bidesh <bideshbanerjee444@gmail.com>
2022-11-28 11:47:16 +0530

    removed unwanted files

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const  cors = require('cors');
 const app = express();
 app.use(cors());
 app.options('*', cors())
-app.options('/posts', cors());
-app.options('/users/getusers', cors())
-app.options('/users/login', cors())
-app.options('/users/getbyId', cors())
-app.options('/users/changeprofile', cors())
-app.options('/stories', cors())
+// app.options('/posts', cors());
+// app.options('/users/getusers', cors())
+// app.options('/users/login', cors())
+// app.options('/users/getbyId', cors())
+// app.options('/users/changeprofile', cors())
+// app.options('/stories', cors())
 app.use(function(req, res, next) { //allow cross origin requests
-    res.setHeader('Access-Control-Allow-Origin', 'https://nightchillins.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
